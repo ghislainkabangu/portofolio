ba9e23116f53c6d8746e935637b0e4b826961fc7
ghislain kabangu <142664005+ghislainkabangu@users.noreply.github.com>
2025-03-04 10:02:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -18,9 +18,9 @@ for (let i = 0; i < sideNav_link.length; i++) {
 }
 
 let header = document.getElementsByTagName("header")[0];
-header.addEventListener("mouseover", ()=>{
-  header.classList.add("headerJSeffet")
-})
-header.addEventListener("mouseout",()=>{
-  header.classList.remove("headerJSeffet")
-})
+header.addEventListener("mouseover", () => {
+  header.classList.add("headerJSeffet");
+});
+header.addEventListener("mouseout", () => {
+  header.classList.remove("headerJSeffet");
+});
